c2095802cb3db6b4b7373a205070ff4ad1cc32b1
0600Hours <tpendley360@gmail.com>
2025-05-09 10:26:01 -0700

    fix 7seg for real

--- public/index.js
+++ public/index.js
@@ -6,15 +6,28 @@ fetch(window.location.href + "status")
     })
     .catch(error => console.log(error));
 
-const create7SegmentDigit = () => {
-    const wrapper = document.createElement("div");
+const create7SegmentDigit = (digit) => {
+    const wrapper = document.createElement("span");
     wrapper.className = "digit-wrapper";
     ["top horiz", "top-left vert", "top-right vert", "middle horiz", "bottom-left vert", "bottom-right vert", "bottom horiz"].forEach((e) => {
         const segment = document.createElement("div");
         segment.className = e;
+        const inner = document.createElement("div");
+        inner.className = "inner";
+        segment.appendChild(inner);
         wrapper.appendChild(segment);
     });
+    if (digit !== undefined) {
+        if (typeof digit === "number") {
+            wrapper.classList.add(`digit-${digit}`);
+        } else {
+            wrapper.classList.add(digit);
+        }
+    }
     return wrapper;
 }
 
-document.getElementById("display").appendChild(create7SegmentDigit());
+document.getElementById("display").appendChild(create7SegmentDigit("minus"));
+for (var i = 0; i <= 9; i++) {
+    document.getElementById("display").appendChild(create7SegmentDigit(i));
+}
