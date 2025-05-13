b45ceb991e345c32792260ac85bb5d8c944ae2e3
0600Hours <tpendley360@gmail.com>
2025-05-12 21:44:27 -0700

    style index page

--- public/index.js
+++ public/index.js
@@ -1,8 +1,34 @@
 const USERS = ["Dax", "Latte", "RelaxingDragon", "Rory", "Takula", "Zingy"];
 
 USERS.forEach((user) => {
+    const linkWrapper = document.createElement("div");
+    linkWrapper.className = "link-wrapper";
     const link = document.createElement("a");
+    link.className = "link"
     link.href = `/${user.toLowerCase()}.html`;
-    link.innerHTML = user;
-    document.getElementById("links-container").appendChild(link);
+
+    const capitals = user.match(/[A-Z]/g);
+    if (capitals.length <= 1) {
+        link.innerHTML = user;
+    } else {
+        let _user = user;
+        let userIndex = 0, capsIndex = 0;
+        while (capsIndex < capitals.length) {
+            console.log(userIndex);
+            console.log(capsIndex);
+            userIndex = _user.indexOf(capitals[capsIndex], userIndex);
+            console.log(userIndex);
+            if (capsIndex > 0) {
+                _user = _user.slice(0, userIndex) + '\u200B' + _user.slice(userIndex);
+            }
+            capsIndex++;
+            console.log("---");
+        }
+        link.innerHTML = _user;
+    }
+
+
+
+    linkWrapper.appendChild(link);
+    document.getElementById("links-container").appendChild(linkWrapper);
 });
