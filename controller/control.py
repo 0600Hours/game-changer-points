4594e35f960e4a13d434688a068efa37c9eaf8a8
0600Hours <tpendley360@gmail.com>
2025-05-15 22:54:35 -0700

    randomize db scores

--- controller/control.py
+++ controller/control.py
@@ -1,5 +1,6 @@
-import sqlite3
 import os
+import random
+import sqlite3
 
 dirname = os.path.dirname(__file__)
 DB_FILE = os.path.join(dirname, "../shared/points.db")
@@ -7,11 +8,12 @@ DB_FILE = os.path.join(dirname, "../shared/points.db")
 contestants = ["Dax", "Latte", "RelaxingDragon", "Rory", "Takula", "Zingy"]
 
 def main():
-    print(list(zip(contestants, [0] * len(contestants))))
     con = sqlite3.connect(DB_FILE)
     db = con.cursor()
     db.execute("CREATE TABLE IF NOT EXISTS points (contestant TEXT PRIMARY KEY, score INTEGER NOT NULL);")
     db.executemany("INSERT OR IGNORE INTO points (contestant, score) VALUES (?, ?)", list(zip(contestants, [0] * len(contestants))))
+
+    db.executemany("UPDATE points SET score = ? WHERE contestant = ?", list(zip(random.sample(range(-999, 9999), len(contestants)), contestants)))
     
     users = db.execute("SELECT * FROM points")
     print(users.fetchall())
